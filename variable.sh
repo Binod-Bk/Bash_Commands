bf2922160cad80fc66d17729d7dec92272afe626
Binod-Bk <gautambinod115@gmail.com>
2024-01-11 23:49:36 +0545

    Variable Basic

---
+++ variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+name="Binod"
+
+echo "Good Morning $name!!"
+sleep 1
+echo "Hello"
+sleep 1
+echo "It's been long that I have seen you $name"
+sleep 1
+echo "I just have been busy these days"
