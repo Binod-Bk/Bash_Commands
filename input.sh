b38e5cf662eb2815b9efa6f82cd18b804b1cf148
Binod-Bk <gautambinod115@gmail.com>
2024-01-12 00:04:44 +0545

    Reading Input

---
+++ input.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "What is your Name"
+
+read name
+
+echo "Good Morning $name!!"
+sleep 1
+echo "Hello"
+sleep 1
+echo "It's been long that I have seen you $name"
+sleep 1
+echo "I just have been busy these days"
