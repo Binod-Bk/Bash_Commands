c5c9a5af608ddc0aa960ef15ef324cceb16e2bee
Binod-Bk <gautambinod115@gmail.com>
2024-01-13 00:02:42 +0545

    Calculate Age

---
+++ millionaire.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "What is your Name?"
+read name
+echo "What is you Age?"
+read age
+echo "Hello $name, please wait..."
+
+echo "Calculating......"
+sleep 2
+echo ".........."
+sleep 2
+echo ".........."
+sleep 2
+calculate=$((($RANDOM%15)+$age))
+echo "$name, you will be a millionaire when you are $calculate years old."
+
