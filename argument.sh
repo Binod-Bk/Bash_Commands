60d5179e58c9e9c6e9dd60485d18e296fdc04ab8
Binod-Bk <gautambinod115@gmail.com>
2024-01-12 23:31:47 +0545

    Argument

---
+++ argument.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+name = $1
+sports =$2
+
+echo "Hello $name"
+sleep 2
+echo "Hello"
+sleep 2
+echo "I have a favour to ask"
+sleep 2
+echo "Go on"
+sleep 2
+echo "Can I get you $sports shoes"
+sleep 2
+echo "Sure"
