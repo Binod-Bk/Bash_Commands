b264dc0b8198257f7dfa97ad28878cd8eee84534
Binod-Bk <gautambinod115@gmail.com>
2024-01-13 22:41:47 +0545

    Your commit message here

--- git_push.sh
+++ git_push.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo 
+function git_push {
+    message=$1
+
+    git add .
+
+    git commit -m "$message"
+
+    git push origin main
+}
+
+
+commit_message="Your commit message here"
+git_push "$commit_message"
+
