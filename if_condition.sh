584294e44d5e171e15f42dbc235aef03c971fb0f
Binod-Bk <gautambinod115@gmail.com>
2024-01-14 23:56:23 +0545

    If Condition

---
+++ if_condition.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+echo "Do you want to watch movie today? (y/n)"
+
+read answer
+
+if[[$answer == "y"]]; then
+	echo "Okay then lets meet at evening."
+else
+	echo "Okay then lets plan for next week"
+
+fi
+
+
