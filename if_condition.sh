2cfb76e7c9b63ae509d17b3ecf4e0204392b4f21
Binod-Bk <gautambinod115@gmail.com>
2024-01-14 23:58:11 +0545

    If Else Condition

--- if_condition.sh
+++ if_condition.sh
@@ -5,7 +5,7 @@ echo "Do you want to watch movie today? (y/n)"
 
 read answer
 
-if[[$answer == "y"]]; then
+if [[ $answer == "y" ]]; then
 	echo "Okay then lets meet at evening."
 else
 	echo "Okay then lets plan for next week"
