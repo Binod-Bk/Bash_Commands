c965d5da4c55909d7e25cdfbf6c39de7197e9f19
Binod-Bk <gautambinod115@gmail.com>
2024-01-15 22:22:59 +0545

    Nested If Else

--- nest_if.sh
+++ nest_if.sh
@@ -5,11 +5,11 @@ echo "Do you want to see if number if Odd or Even?(y/n)"
 
 read ans
 
-if [[ $ans == "y" ]] then
+if [[ $ans == "y" ]]then
 
 	echo "Enter the number of your Desire."
 	read num
-	if [[ $num%2 == 0 ]] then
+	if [[ $num%2 == 0 ]]then
 		echo "The number $num is Even."
 	else
 		echo "The number $num is Odd."
