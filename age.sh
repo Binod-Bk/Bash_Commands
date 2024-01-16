da4339fb76b1abd82c587111db97db0b14cefc79
Binod-Bk <gautambinod115@gmail.com>
2024-01-16 10:26:02 +0545

    Elif

--- age.sh
+++ age.sh
@@ -8,7 +8,7 @@ if (( $age < 10 )); then
 	echo "You are a Kid."
 elif (( $age >= 10 && $age < 20  )); then
 	echo "You are a Teen."
-elif (( $age >= 20 && $age > 60  )); then
+elif (( $age >= 20 && $age < 60  )); then
 	echo "You are a Adult."
 else
 	echo "You are an Elder."
